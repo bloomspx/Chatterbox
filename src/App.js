11fe505a8c3d270fc95c8c5d1287913f017031b5
bloomspx <bloomspx@gmail.com>
2022-10-25 16:34:57 +0800

    new simple fetch method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Error from "./pages/error/Error"
 import Home from "./pages/home/Home"
 import Dashboard from "./pages/dashboard/Dashboard";
 import Summary from "./pages/summary/Summary";
+import Test from "./pages/test/Test";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/dashboard' element = {<Dashboard/>}/>
         <Route path='/upload-video' element = {<UploadSTT/>}/>
         <Route path='/upload-text' element = {<UploadTA/>}/>
+        <Route path='/test' element = {<Test/>}/>
         <Route path='/text-analysis' element = {<TextAnalysis/>}/>
         <Route path='/speech-to-text' element = {<SpeechToText/>}/>
         <Route path='/summary' element = {<Summary/>}/>
